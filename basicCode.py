7daea2010cf633163c20ab2d75fa9ab19e9f7be6
Pappu sanodiya <81160267+pappusanodiya@users.noreply.github.com>
2023-04-06 19:39:14 +0530

    Create basicCode.py

---
+++ basicCode.py
@@ -0,0 +1,38 @@
+s = "linux world"
+
+#reverse the string
+s[::-1]
+s.capitalize()
+"linux" in s
+
+
+email = [
+    "vima.com",
+    "vipar.com",
+    "thap,com",
+    "this,com",
+    "fdge.com"
+]
+email
+email[1:6:2]
+email.append("thisi.com")
+email.reverse()
+db = [2,3,4,5]
+
+db = [ 1,2,3,4,5,6,7]
+db[6]
+for i in db:
+    print(i*i)
+    
+   
+salary= 9
+if salary >= 10:
+    print("i will join job")
+else:
+    print("i will not join job")
+    
+    
+    if bool ( print("linux")):
+    print("linux")
+else:
+    print("world")
